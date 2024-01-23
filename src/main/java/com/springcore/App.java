28a29cf88a4bc3cbd8fd73febcc95ed086aa3e9d
Nityanand Singh <nns.nitya@gmail.com>
2024-01-23 16:16:21 +0530

    Initial commit

---
+++ src/main/java/com/springcore/App.java
@@ -0,0 +1,19 @@
+package com.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApplicationContext context=new ClassPathXmlApplicationContext("config_core.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student);
+    }
+}
