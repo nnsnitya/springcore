fe4f8b36ee9480eabcc1e744fccd0673a67380db
Nityanand Singh <nns.nitya@gmail.com>
2024-01-23 16:38:58 +0530

    adding properties Injection
    using p schema as well

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -13,7 +13,9 @@ public class App
     {
         System.out.println( "Hello World!" );
         ApplicationContext context=new ClassPathXmlApplicationContext("config_core.xml");
-        Student student = (Student) context.getBean("student");
-        System.out.println(student);
+        Student student1 = (Student) context.getBean("student1");
+        System.out.println(student1);
+        Student student2 = (Student) context.getBean("student2");
+        System.out.println(student2);
     }
 }
