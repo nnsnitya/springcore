1ed2d3cc4d847eb0c3b85590f8176852928a2b71
Nityanand Singh <nns.nitya@gmail.com>
2024-01-23 18:21:40 +0530

    Ambiguity Problem2
    calling constructor with double parameter as it is on top

--- src/main/java/com/springcore/ci/Addition.java
+++ src/main/java/com/springcore/ci/Addition.java
@@ -4,15 +4,15 @@ public class Addition {
 	private int a;
 	private int b;
 	
-	public Addition(int a, int b) {
-		this.a=a;
-		this.b=b;
-		System.out.println("Addition Constructor : int, int");
-	}
 	public Addition(double a, double b) {
 		this.a=(int)a;
 		this.b=(int)b;
 		System.out.println("Addition Constructor : double, double");
+	}
+	public Addition(int a, int b) {
+		this.a=a;
+		this.b=b;
+		System.out.println("Addition Constructor : int, int");
 	}
 	
 	public void doSum() {
