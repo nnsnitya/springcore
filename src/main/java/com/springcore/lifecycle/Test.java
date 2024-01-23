ebc2de3edb77ab64b1e8c4348553f6ae213b90b6
Nityanand Singh <nns.nitya@gmail.com>
2024-01-23 18:53:33 +0530

    register shutdown hook
    to call destroy method need to add shutdown hook which is available in AbstractApplicationContext class

--- src/main/java/com/springcore/lifecycle/Test.java
+++ src/main/java/com/springcore/lifecycle/Test.java
@@ -1,15 +1,17 @@
 package com.springcore.lifecycle;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 
 	public static void main(String[] args) {
-		ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/lifecycle/config_lifecycle.xml");
+		AbstractApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/lifecycle/config_lifecycle.xml");
 		Samosa s1=(Samosa) context.getBean("s1");
 		System.out.println(s1);
 		
+		//registering shutdown hook
+		context.registerShutdownHook();
 	}
 
 }
