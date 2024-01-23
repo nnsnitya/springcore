cf0992f786cc3a867fa5fc27941ef893dd48be03
Nityanand Singh <nns.nitya@gmail.com>
2024-01-24 00:57:11 +0530

    AutowiredAnno: Qualifier
    can specify the qualifier name to switch from byType to byName while using @Autowired on property

--- src/main/java/com/springcore/autowire/annotation/Emp.java
+++ src/main/java/com/springcore/autowire/annotation/Emp.java
@@ -1,9 +1,11 @@
 package com.springcore.autowire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Emp {
-	
+	@Autowired
+	@Qualifier("temp1")
 	private Address address;
 
 	
@@ -12,7 +14,6 @@ public class Emp {
 		// TODO Auto-generated constructor stub
 	}
 	
-	@Autowired
 	public Emp(Address address) {
 		super();
 		this.address = address;
