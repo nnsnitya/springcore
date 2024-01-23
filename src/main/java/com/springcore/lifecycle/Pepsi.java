659673c01bb11f2248f10cd29f4a9f3c2d128663
Nityanand Singh <nns.nitya@gmail.com>
2024-01-23 19:06:37 +0530

    adding InitializatingBean & DisposableBean
    to manage init/destroy, adding InitializatingBean & DisposableBean

---
+++ src/main/java/com/springcore/lifecycle/Pepsi.java
@@ -0,0 +1,37 @@
+package com.springcore.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Pepsi implements InitializingBean, DisposableBean {
+	private double price;
+
+	
+	public Pepsi() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Pepsi [price=" + price + "]";
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Taking pepsi: init");
+	}
+
+	public void destroy() throws Exception {
+		System.out.println("Going to put bottle back to shop after drinking : Destroy");
+	}
+	
+	
+}
--- src/main/java/com/springcore/lifecycle/Test.java
+++ src/main/java/com/springcore/lifecycle/Test.java
@@ -7,11 +7,15 @@ public class Test {
 
 	public static void main(String[] args) {
 		AbstractApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/lifecycle/config_lifecycle.xml");
+		//registering shutdown hook
+		context.registerShutdownHook();
+		
 		Samosa s1=(Samosa) context.getBean("s1");
 		System.out.println(s1);
 		
-		//registering shutdown hook
-		context.registerShutdownHook();
+		System.out.println("++++++++++++++++++++++++");
+		Pepsi p1 = (Pepsi) context.getBean("p1");
+		System.out.println(p1);
 	}
 
 }
