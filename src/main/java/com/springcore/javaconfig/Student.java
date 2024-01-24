bab7cc3ca03a062fb6d00e5ecf8c7873bae24d38
Nityanand Singh <nns.nitya@gmail.com>
2024-01-24 20:52:23 +0530

    javaconfig: removing xml file
    by adding JavaConfig class annotating with @Configuration
    Also need to add Subclass AnnotationConfigApplicationContext

--- src/main/java/com/springcore/javaconfig/Student.java
+++ src/main/java/com/springcore/javaconfig/Student.java
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Component;
 @Component("firststudent")
 public class Student {
 	
+	public void study() {
+		System.out.println("Student is reading book");
+	}
+	
 }
